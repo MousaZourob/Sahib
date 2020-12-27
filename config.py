1aa48ea97b1eeb2083e50ac2265ac08e051dc5db
MousaZourob <mousazourob@gmail.com>
2020-12-27 18:43:00 -0500

    added skeleton files

---
+++ config.py
@@ -0,0 +1,3 @@
+bestbuy_base_url = 'https://www.bestbuy.ca/en-ca'
+chrome_driver = 'Drivers/geckodriver.exe'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'

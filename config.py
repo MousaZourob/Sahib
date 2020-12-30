e4e4066dded7924267908928960ef606a2429da9
MousaZourob <mousazourob@gmail.com>
2020-12-29 19:03:50 -0500

    reformated code

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 walmart_base_url = 'https://www.walmart.ca/en'
 gecko_driver = 'Drivers/geckodriver.exe'
+
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+page_load = 30

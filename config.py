96704c3a754a8470b424cde5b7274a0e540d26e2
MousaZourob <mousazourob@gmail.com>
2021-01-04 13:02:32 -0500

    finished implementing cookies to login

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ gecko_driver = 'Drivers/geckodriver.exe'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
 page_load = 30
 
+twitter_url = 'https://twitter.com'
 twitter_login_page = 'https://twitter.com/login'
 twitter_cookies_path = 'twitter_cookies.json'
